417a8d7a51fb9cccae9baa993bf87f562e131eb0
WuytsW <willem.wuyts@student.kuleuven.be>
2026-05-09 15:32:33 +0200

    sparql-feedback agent

--- model/agent.py
+++ model/agent.py
@@ -1,15 +1,7 @@
-from typing import List, Tuple, Annotated, TypedDict
-import operator
+from typing import TypedDict
 
 
 class PlanExecute(TypedDict):
     input: str
-    plan: List[str]
-    past_steps: Annotated[List[Tuple], operator.add]
-    intermediate_steps: Annotated[List[Tuple], operator.add]
     chat_history: list
     response: str
-    feedback_task: str
-    gave_feedback: bool
-    feedback_has_results: bool
-    feedback_is_timeout: bool
